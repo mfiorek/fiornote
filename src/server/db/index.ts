d8b28b07a753848a8271f11526adf34419f9bca1
mfiorek <marcin.lukasz.fiorek@gmail.com>
2023-10-22 02:31:37 +0200

    Fix db connection to use mysql2

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,8 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema }
-);
+const connection = await mysql.createConnection(env.DATABASE_URL);
+export const db = drizzle(connection, { schema, mode: "default" });
