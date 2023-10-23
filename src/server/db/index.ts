98c4e3b27e1b25345c8dbe635f353b709c834388
mfiorek <marcin.lukasz.fiorek@gmail.com>
2023-10-23 23:13:37 +0200

    Limit DB connections

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,5 +4,8 @@ import mysql from "mysql2/promise";
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
-const connection = await mysql.createConnection(env.DATABASE_URL);
+const connection = await mysql.createConnection({
+  uri: env.DATABASE_URL,
+  connectionLimit: 1,
+});
 export const db = drizzle(connection, { schema, mode: "default" });
